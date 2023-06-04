0500f2643f39a176a02f09b1ab22bb9107ec93b4
f8b4i2 <45534457+Proger30@users.noreply.github.com>
2023-06-04 21:55:27 +0600

    Update FriendListWidget.jsx

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://socialmediaback-r5tn.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
